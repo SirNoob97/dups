2ff719cbe24d3e62fa22b2b41e449f999d093a6a
SirNoob97 <martinald467@gmail.com>
2022-07-17 21:27:04 +0200

    first andn basic implementation, ignoring .git directory(just for tests)

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+var ignore = []string{".git"}
+
+type md5Table map[string][]string
+
+func isIgnored(dir string) bool {
+	for _, e := range ignore {
+		if e == dir {
+			return true
+		}
+	}
+	return false
+}
+
+func readTree(directory string) (md5Table, error) {
+	table := make(md5Table)
+	walk := func(path string, fInfo os.DirEntry, err error) error {
+		if err != nil && err != os.ErrNotExist {
+			return err
+		}
+
+		if fInfo.Type().IsDir() && isIgnored(fInfo.Name()) {
+			return filepath.SkipDir
+		}
+
+		if fInfo.Type().IsRegular() {
+			hash, file := getHash(path)
+			table[hash] = append(table[hash], file)
+		}
+		return nil
+	}
+
+	return table, filepath.WalkDir(directory, walk)
+}
+
+func getHash(path string) (string, string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		log.Fatal(err)
+	}
+
+	return fmt.Sprintf("%x", hash.Sum(nil)), path
+}
+
+func showOutput(hashTable md5Table) {
+	for hash, files := range hashTable {
+		if len(files) > 1 {
+			fmt.Printf("Files that share the md5 hash: %s\n\n", hash)
+
+			for _, file := range files {
+				fmt.Printf("  %s\n", file)
+			}
+			fmt.Println()
+		}
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Missing required parameter: '<path>'")
+	}
+
+	hashes, err := readTree(os.Args[1])
+	if err == nil {
+		showOutput(hashes)
+	}
+}
