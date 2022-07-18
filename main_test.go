56c43307c2de3cbcb70a0132d17f0813fe9b69fb
SirNoob97 <martinald467@gmail.com>
2022-07-18 18:05:08 +0200

    empty file constants and one basic test for getHash

--- main_test.go
+++ main_test.go
@@ -6,7 +6,11 @@ import (
 	"time"
 )
 
-const TEST_DATA = "testdata"
+const (
+	TEST_DATA           = "testdata"
+	EMPTY_FILE_PATH     = TEST_DATA + "/empty_file"
+	EMPTY_FILE_MD5_HASH = "d41d8cd98f00b204e9800998ecf8427e"
+)
 
 // Test_IsIgnore_True_WhenDirectoryIsOnTheList
 // '.git' director is ignored by default
@@ -55,5 +59,14 @@ func Test_ReadTree_EmptyMapAndNonNil_WhenDirectoryDoesntExists(t *testing.T) {
 	if len(table) > 0 && err == nil {
 		t.Fatalf("Expected an empty Map got with len of %d", len(table))
 		t.Fatal("Expected a non 'nil' error")
+	}
+}
+
+// Test_GetHash_TowNonZeroLenString_WhenFileExists ...
+func Test_GetHash_TowNonZeroLenString_WhenFileExists(t *testing.T) {
+	hash, path := getHash(EMPTY_FILE_PATH)
+
+	if len(hash) == 0 && len(path) == 0 {
+		t.Fatal("Expected two string with non zero length")
 	}
 }
