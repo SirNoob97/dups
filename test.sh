c393ee931200b498ee687524c8448f5a2e137bae
SirNoob97 <martinald467@gmail.com>
2022-07-18 18:04:33 +0200

    improve test files content and add empty file

--- test.sh
+++ test.sh
@@ -6,8 +6,9 @@ test_dir="./testdata"
 
 function setup_test_dir {
     mkdir -p $test_dir/{1..5}/{a..e}
-    touch $test_dir/{test_{1..5},{1..5}/{test_{1..5},{a..e}/test_{a..e}}}
-    echo "this is a test file" | tee $test_dir/{test_{1,3,5},{1..3}/{test_{1..3},{a..c}/test_{a,b,e}}} 1>/dev/null
+    touch $test_dir/{test_{1..5},empty_file,{1..5}/{test_{1..5},{a..e}/test_{a..e}}}
+    echo "this is a test file" | tee $test_dir/{test_{1..3},{1..5}/{test_{1..3},{a..e}/test_{a..c}}} 1>/dev/null
+    echo "this is a another test file" | tee $test_dir/{test_{4,5},{1..5}/{test_{4,5},{a..e}/test_{d,e}}} 1>/dev/null
 }
 
 if [ ! -d $test_dir ]; then
